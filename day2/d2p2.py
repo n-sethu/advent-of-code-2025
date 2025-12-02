a610e8af4fe0cd8478cc9937601e034e5f84e29f
Nikhil <107327738+n-sethu@users.noreply.github.com>
2025-12-02 17:17:26 -0500

    day 2 part 2 complete

--- day2/d2p2.py
+++ day2/d2p2.py
@@ -2,11 +2,27 @@ file = open("input.txt","r",newline ="")
 ans =0
 def isRepeat(line:str)->bool:
     n = len(line)
-    for i in range(1,n//2 +1):
-        
+    if n==1:
         return False
-    return True
+    for i in range(1,n//2 +1):
+        if n%i!=0:
+            continue
+        mult = n//i
+        if line == line[:i]*mult:
+            return True
+    return False
+
     
+
 
 line = file.readline()
 line_list = line.split(',')
+for word in line_list:
+    spl = word.split('-')
+    start = int(spl[0].strip())
+    end = int(spl[1].strip())
+    for i in range(start,end+1):
+        to_check = str(i)
+        if isRepeat(to_check):
+            ans+=i
+print(ans)
